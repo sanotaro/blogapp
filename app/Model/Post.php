38c8150fbf7ee916fb759cc63bc7aa5efc3ceba7
Taro Sano <mightyboy0417@gmail.com>
2016-01-28 23:38:33 +0900

    Postのモデル、Postのテスト追加

---
+++ app/Model/Post.php
@@ -0,0 +1,30 @@
+<?php
+App::uses ( 'AppModel', 'Model' );
+/**
+ * Post Model
+ */
+class Post extends AppModel {
+
+	/**
+	 * バリデーションルール
+	 *
+	 * @var array
+	 */
+	public $validate = [
+			'title' => [
+					'notEmpty' => [
+							'rule' => [
+									'notEmpty'
+							],
+							'message' => 'タイトルは必須入力です'
+					],
+					'maxLength' => [
+							'rule' => [
+									'maxLength',
+									'255'
+							],
+							'message' => 'タイトルは255文字以内で入力してください'
+					]
+			]
+	];
+}
