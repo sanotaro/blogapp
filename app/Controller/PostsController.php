60aa5f16754be9023f3c5d4cfcbd03d766f49ecf
Taro Sano <mightyboy0417@gmail.com>
2016-01-31 23:42:05 +0900

    余計だった $this->Session->setFlashの引数を削除

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -18,18 +18,12 @@ class PostsController extends AppController {
 			$this->Post->create($this->request->data);
 			if ($this->Post->save()) {
 				$this->Session->setFlash(
-						__('新しい記事を受け付けました。'),
-						'alert',
-						['plugin' => 'BoostCake', 'class' => 'alert-success']
-				);
+						__('新しい記事を受け付けました。'));
 
 				return $this->redirect(['action' => 'index']);
 			} else {
 				$this->Session->setFlash(
-						__('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'),
-						'alert',
-						['plugin' => 'BoostCake', 'class' => 'alert-success']
-				);
+						__('記事の投稿に失敗しました。入力内容を確認して再度投稿してください。'));
 			}
  		}
 	}
